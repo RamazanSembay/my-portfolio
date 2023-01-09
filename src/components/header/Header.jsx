113c8397f022b1579bd9cb5c6700b2744f65e83b
RamazanSembay <ramazansembai01@gmail.com>
2023-01-10 03:55:50 +0600

    my

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const Social = () => {
+const Header = () => {
   return (
     <div>
-      Social
+      Header
+
     </div>
   )
 }
 
-export default Social
+export default Header
