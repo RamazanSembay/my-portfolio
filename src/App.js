5a605e5116e36c7d31b091582f7d23048c8e9e12
RamazanSembay <ramazansembai01@gmail.com>
2023-01-10 04:00:53 +0600

    my

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import About from './components/about/About';
 
 function App() {
   return (
