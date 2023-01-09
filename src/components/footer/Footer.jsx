34c03db70d23370149e1652eb1c7fc8cd830b121
RamazanSembay <ramazansembai01@gmail.com>
2023-01-10 04:04:55 +0600

    my

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className='footer'>
+        <div className='footer__container container'>
+          <h1 className='footer__title'>Nurramazan Sembay</h1>
+          <ul className='footer__list'>
+            <li>
+              <a href="#about" className='footer__link'>About</a>
+            </li>
+
+            <li>
+              <a href="#projects" className='footer__link'>Projects</a>
+            </li>
+
+            <li>
+              <a href="#" className='footer__link'></a>
+            </li>
+          </ul>
+
+          <div className='footer__social'>
+            <a
+              href="https://ru-ru.facebook.com/"
+              className='footer__social-link' target="_blank">
+              <i class='bx bxl-facebook'></i>
+            </a>
+
+            <a
+              href="https://www.instagram.com/nurramazan_sembayev/"
+              class='footer__social-link' target="_blank">
+              <i class="bx bxl-instagram"></i>
+            </a>
+
+            <a
+              href="https://twitter.com/?lang=ru"
+              className='footer__social-link' target="_blank">
+              <i class='bx bxl-twitter'></i>
+            </a>
+          </div>
+
+          <span className='footer__copy'>&#169; Nurramazan. All rights reserved</span>
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 
 const Social = () => {
-  return (
-    <div>
-      Social
-    </div>
-  )
+    return (
+        <div className='home__social'>
+            <a
+                href="https://www.instagram.com/nurramazan_sembayev/"
+                className='home__social-icon' target="_blank">
+                <i class='uil uil-instagram'></i>
+            </a>
+
+            <a
+                href="https://vk.com/nurramazan_sembayev"
+                class='home__social-icon' target="_blank">
+                <i class="uil uil-vk-alt"></i>
+            </a>
+
+            {/* <a
+                href="https://github.com/"
+                className='home__social-icon' target="_blank">
+                <i class='uil uil-github-alt'></i>
+            </a> */}
+        </div>
+    )
 }
 
 export default Social
