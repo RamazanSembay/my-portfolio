45f130b8a1172041159e1a09feaad96ffc88713c
RamazanSembay <ramazansembai01@gmail.com>
2023-01-11 16:34:50 +0600

    my

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -6,19 +6,19 @@ const Social = () => {
             <a
                 href="https://www.instagram.com/nurramazan_sembayev/"
                 className='home__social-icon' >
-                <i class='uil uil-instagram'></i>
+                <i class="uil uil-instagram-alt"></i>
             </a>
 
             <a
                 href="https://vk.com/nurramazan_sembayev"
                 class='home__social-icon' >
-                <i class="uil uil-vk-alt"></i>
+                <i class="uil uil-vk"></i>
             </a>
 
             <a
-                href="https://github.com/"
+                href="https://twitter.com/"
                 className='home__social-icon' >
-                <i class='uil uil-github-alt'></i>
+                <i class="uil uil-twitter"></i>
             </a>
         </div>
     )
