4c8963770432f6e29410093a9be3ff7f792ae2fb
RamazanSembay <ramazansembai01@gmail.com>
2023-01-11 13:51:17 +0600

    my

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -64,7 +64,7 @@ const Data = () => {
             </p>
 
             <a href="#contact" className="button button--flex">
-                Мой успех
+                Хабарласу
                 <svg
                     class="button__icon"
                     xmlns="http://www.w3.org/2000/svg"
